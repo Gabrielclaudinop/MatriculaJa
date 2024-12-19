d8c022d6b307743ed53cec13d27dd94aa295692b
gabrielCP06 <140004409+gabrielCP06@users.noreply.github.com>
2024-12-19 15:28:17 +0000

    email colocado

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -3,6 +3,7 @@ import { login } from '../controllers/login.js';
 import { isAuthenticated } from '../middleware/auth.js';
 import { validate } from '../middleware/validate.js';
 import { z } from 'zod';
+import SendMail from './services/SendMail.js';
 
 const router = express.Router();
 
@@ -17,10 +18,14 @@ router.post('/login',
         }),
       }),
     })
-  ),login)
+  ),
+  login
+  //await SendMail.createNewUser(user.email),
+  );
+
 //ROTA: /auth/validate-auth
 router.post('/validate-auth', isAuthenticated, (req, res) => {
     res.status(200).json({message: 'OK', isValid: true})
-})
+});
 
 export default router;
